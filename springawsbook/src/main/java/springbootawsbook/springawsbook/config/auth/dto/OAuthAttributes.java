56eb0638d616e734052bc92a3856a2a04db1cb16
lkc263 <lkc263@naver.com>
2022-05-20 17:52:35 +0900

    ✏ 소셜 로그인 개선하기 및 네이버 로그인

--- springawsbook/src/main/java/springbootawsbook/springawsbook/config/auth/dto/OAuthAttributes.java
+++ springawsbook/src/main/java/springbootawsbook/springawsbook/config/auth/dto/OAuthAttributes.java
@@ -26,7 +26,7 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
-        if("naver".equals(registrationId)) {
+        if ("naver".equals(registrationId)) {
             return ofNaver("id", attributes);
         }
 
@@ -63,4 +63,6 @@ public class OAuthAttributes {
                 .role(Role.GUEST)
                 .build();
     }
+
+
 }
--- springawsbook/src/main/java/springbootawsbook/springawsbook/web/IndexController.java
+++ springawsbook/src/main/java/springbootawsbook/springawsbook/web/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import springbootawsbook.springawsbook.config.auth.LoginUser;
 import springbootawsbook.springawsbook.config.auth.dto.SessionUser;
 import springbootawsbook.springawsbook.service.PostsService;
 import springbootawsbook.springawsbook.web.dto.PostsResponseDto;
@@ -20,9 +21,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("post", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
